7a64914f4f4957dd4aa8da0422e63cf06ca440e1
reinvvay <reinvvay@gmail.com>
2025-10-26 20:26:18 +0200

    Sol

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,9 @@
+import csv
 from dataclasses import dataclass
 from urllib.parse import urljoin
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
@@ -15,8 +18,34 @@ class Product:
     num_of_reviews: int
 
 
+def get_single_page():
+    driver = webdriver.Chrome()
+    driver.get("https://webscraper.io/test-sites/e-commerce/more/")
+    products = []
+    for single in driver.find_elements(By.CLASS_NAME, "caption"):
+        products.append(Product(
+            title=single.find_element(By.CLASS_NAME, "title"),
+            description=single.find_element(By.CLASS_NAME, "description"),
+            price=single.find_element(By.CSS_SELECTOR, "[itemprop='price']"),
+            rating=single.find_element(By.CSS_SELECTOR, "[itemprop='data-rating]'").get_attribute("data-rating"),
+            num_of_reviews=single.find_element(By.CSS_SELECTOR, "[itemprop='reviewCount']"),
+        ))
+
+    return products
+
+
+def write_to_csv(output_file):
+    with open(output_file, "w", encoding="utf-8", newline="") as csv_file:
+        products = get_single_page()
+        writer = csv.writer(csv_file)
+        for product in products:
+            writer.writerow(
+                [product.title, product.description, product.price, product.rating, product.num_of_reviews]
+            )
+
+
 def get_all_products() -> None:
-    pass
+    write_to_csv("test.csv")
 
 
 if __name__ == "__main__":
